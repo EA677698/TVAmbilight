022a694dd171ae4b819f15cc74ebee3857e4b877
Erick Alanis <erickalanis@pop-os.localdomain>
2023-02-23 03:28:19 -0700

    More correct Math, refresh rates and video format

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <opencv2/videoio.hpp>
 #include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
 #include "ws2811/ws2811.h"
 #include <signal.h>
 #include <unistd.h>
@@ -9,6 +10,7 @@
 #define GPIO_PIN 18
 #define DMA 10
 #define RGB_ORDER WS2811_STRIP_GRB
+#define DEFAULT_REFRESH_RATE 60
 
 using namespace std;
 using namespace cv;
@@ -41,18 +43,6 @@ ws2811_t led_strip =
 
 VideoCapture rca;
 
-void sig_kill_handler(int signum) {
-    printf("\nProcess closing...\n");
-    ws2811_fini(&led_strip);
-    rca.release();
-    if(!rca.isOpened()){
-        printf("Video stream closed successfully!\n");
-    } else{
-        fprintf(stderr,"Error: Failed to close video stream!\n");
-    }
-    printf("Process gracefully closed\n");
-    exit(0);
-}
 
 void setBrightness(unsigned short b) {
     led_strip.channel[0].brightness = b;
@@ -67,8 +57,31 @@ void setPixelColorRGB(int pixel, unsigned short r, unsigned short g, unsigned sh
     }
 }
 
+void sig_kill_handler(int signum) {
+    printf("\nProcess closing...\n");
+    for(int i = 0; i<LED_COUNT; i++){
+        setPixelColorRGB(i,0,0,0);
+    }
+    ws2811_fini(&led_strip);
+    rca.release();
+    if(!rca.isOpened()){
+        printf("Video stream closed successfully!\n");
+    } else{
+        fprintf(stderr,"Error: Failed to close video stream!\n");
+    }
+    printf("Process gracefully closed\n");
+    exit(0);
+}
+
 int main(int argc, char **argv) {
+    if(argc < 2){
+        fprintf(stderr,"Invalid arguments\n"
+                       "Usage: ./TVAmbilight [Video device ID] [Optional: refresh rate, default = 60]");
+        return 0;
+    }
     char* deviceID = *(argv+1);
+    int refresh_rate;
+
     signal(SIGINT, sig_kill_handler);
     ws2811_init(&led_strip);
     Mat frame;
@@ -80,6 +93,15 @@ int main(int argc, char **argv) {
             return 1;
         }
     }
+    if(argc < 3){
+        refresh_rate = 1000/DEFAULT_REFRESH_RATE;
+        rca.set(CAP_PROP_FPS,DEFAULT_REFRESH_RATE);
+    } else{
+        refresh_rate = 1000/stoi(*(argv+2));
+        rca.set(CAP_PROP_FPS,stoi(*(argv+2)));
+    }
+
+    rca.set(CAP_PROP_FOURCC,VideoWriter::fourcc('Y','U','Y','V'));
     rca.read(frame);
     const unsigned short rows = frame.rows;
     const unsigned short cols = frame.cols;
@@ -87,6 +109,7 @@ int main(int argc, char **argv) {
     int lengths = LED_COUNT / 4;
     char attempts = 0;
     unsigned char *temp;
+    int pixel;
     while (1) {
         if (frame.empty()) {
             fprintf(stderr, "Error: Missing frame\n");
@@ -101,20 +124,25 @@ int main(int argc, char **argv) {
             continue;
         }
         attempts = 0;
-        for (int i = skip; i < cols; i += skip) {
-            temp = frame.ptr<unsigned char>(0, i);
-            setPixelColorRGB(i - 4, temp[0], temp[1], temp[2]);
-            temp = frame.ptr<unsigned char>(rows - 1, i);
-            setPixelColorRGB((lengths * 2) + (i - 4), temp[0], temp[1], temp[2]);
+        pixel = skip;
+        for (int i = 0; i < lengths; i++) {
+            temp = frame.ptr<unsigned char>(0, skip);
+            setPixelColorRGB(i, temp[0], temp[1], temp[2]);
+            temp = frame.ptr<unsigned char>(rows - 1, skip);
+            setPixelColorRGB((lengths * 2) + i, temp[0], temp[1], temp[2]);
+            pixel += skip;
         }
-        for (int i = skip; i < rows; i += skip) {
-            temp = frame.ptr<unsigned char>(i, 0);
-            setPixelColorRGB(lengths + (i - 4), temp[0], temp[1], temp[2]);
-            temp = frame.ptr<unsigned char>(i, cols - 1);
-            setPixelColorRGB((lengths * 3) + (i - 4), temp[0], temp[1], temp[2]);
+        pixel = skip;
+        for (int i = 0; i < lengths; i++) {
+            temp = frame.ptr<unsigned char>(skip, 0);
+            setPixelColorRGB(lengths + i, temp[0], temp[1], temp[2]);
+            temp = frame.ptr<unsigned char>(skip, cols - 1);
+            setPixelColorRGB((lengths * 3) + i, temp[0], temp[1], temp[2]);
+            pixel += skip;
         }
         ws2811_render(&led_strip);
         rca.read(frame);
+        waitKey(refresh_rate);
 
     }
 
